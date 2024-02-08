d4bdf509e1302861d0ada6ce58905586240d2ac1
MoojinKim <moojin.kim@student.manchester.ac.uk>
2024-02-08 16:43:17 +0000

    Debugger

--- hello_world.c
+++ hello_world.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
-
-int main () {
-   
-   printf("Hello World");
-   
-   return(0);
-}
+#include <stdio.h> // directive to include 
+ 
+int main(void)   // function which is run when the programs starts 
+{ 
+printf("Hello World"); // our statement/command to run 
+ 
+int i = 0; // make a variable called i 
+i = i + 3; 
+ 
+    return 0; // the return statement for the function main 
+} 
